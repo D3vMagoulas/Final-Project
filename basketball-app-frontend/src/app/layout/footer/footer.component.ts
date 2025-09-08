7871e94b065eaa87c375847cf924d1d9b41adffb
D3vMagoulas <magoulas.dev@gmail.com>
2025-09-08 19:07:19 +0300

    Updated UI

--- basketball-app-frontend/src/app/layout/footer/footer.component.ts
+++ basketball-app-frontend/src/app/layout/footer/footer.component.ts
@@ -3,13 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  template: `
-  <footer class="footer">
-    <div class="inner">© {{year}} Ίκαρος Νέας Σμύρνης</div>
-  </footer>`,
-  styles:[`
-    .footer{height:var(--footer-height);background:#0b0b0b;color:#bbb;display:flex;align-items:center;justify-content:center}
-    .inner{max-width:1200px;margin:0 auto;padding:0 1rem;text-align:center}
-  `]
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent { year = new Date().getFullYear(); }
+export class FooterComponent {
+  year = new Date().getFullYear();
+}
