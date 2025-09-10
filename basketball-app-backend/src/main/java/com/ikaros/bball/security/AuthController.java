c364294d28d63d297508bc4f658e36512d8df2cf
D3vMagoulas <magoulas.dev@gmail.com>
2025-09-10 18:34:40 +0300

    Hotfix

--- basketball-app-backend/src/main/java/com/ikaros/bball/security/AuthController.java
+++ basketball-app-backend/src/main/java/com/ikaros/bball/security/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.*;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.AuthenticationException;
 
 import java.net.URI;
 
